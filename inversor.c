d180b75a2aab894f84c6e6b54e64a78bfd6ef8ea
stackiller <david.vegasc@gmail.com>
2020-05-24 22:15:33 -0300

    feito

---
+++ inversor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Simples conversor de maiúsculas,
+ * minúsculas e vice-versa */
+
+char
+toLower(char ch) {
+  if(ch >= 65 && ch <= 90)
+    return ch+32;
+  else if(ch >= 97 && ch <= 122 )
+    return ch-32;
+  return ch;
+}
+
+int
+main(int argc, char **argv) {
+  for(int s=1; s < argc; s++) {
+    for(int j=0; j < argv[s][j]; j++)
+      printf("%c", toLower(argv[s][j]));
+    putchar(32);
+  }
+  putchar(10);
+}
+    
